04e0a1bed6ea750716950dc21a7f10e1d8602b63
peppedelninno44-art <peppedelninno44@gmail.com>
2026-07-15 22:23:33 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -3,7 +3,6 @@ from discord.ext import commands
 import os
 import asyncio
 
-# Configurazione permessi
 intents = discord.Intents.default()
 intents.members = True 
 intents.message_content = True
@@ -14,7 +13,6 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 async def on_ready():
     print(f'Bot is online as {bot.user}')
 
-# Welcome message (lasciamo come prima)
 @bot.event
 async def on_member_join(member):
     channel = bot.get_channel(1516927038794240161)
@@ -24,17 +22,22 @@ async def on_member_join(member):
             description=f'Hey {member.mention}, welcome to the ring! Get ready to fight!',
             color=discord.Color.red()
         )
-        embed.set_image(url="https://media2.giphy.com/media/v1.Y2lkPTZjMDliOTUyazJweXM1eGZiNWtmb3ZycDN6b3kyMHlydmhtd3lxNjUxcTc4czhtZSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/2ZpkYucNiZpovyuMkf/giphy.gif")
+        embed.set_image(url="https://c.tenor.com/tXFv497uQ5sAAAAC/ashitano-joe-joe-yabuki.gif")
         await channel.send(content=f'New challenger: {member.mention}', embed=embed)
 
-# Goodbye message (MODIFICATO: invio come file per evitare blocchi)
 @bot.event
 async def on_member_remove(member):
     channel = bot.get_channel(1516927038794240161)
     if channel:
-        # Messaggio di testo invece di Embed per l'immagine
-        await channel.send(f'Challenger retired: {member.name} has left the ring. See you next time!')
-        # Invio diretto della GIF come file
-        await channel.send("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjExNDMwJnc9YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7TKMGpxxHOGTdzJC/giphy.gif")
+        embed = discord.Embed(
+            title="Challenger retired",
+            description=f'{member.name} has left the ring. See you next time!',
+            color=discord.Color.dark_grey()
+        )
+        # Questa è una GIF di Joe che se ne va, molto stabile
+        embed.set_image(url="https://c.tenor.com/2s787LqG9_IAAAAC/joe-yabuki-ashita-no-joe.gif")
+        
+        await asyncio.sleep(1)
+        await channel.send(embed=embed)
 
 bot.run(os.environ['TOKEN'])
