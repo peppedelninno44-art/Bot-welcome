0c3639419c08c8418fd3443b872282a5781ef662
peppedelninno44-art <peppedelninno44@gmail.com>
2026-07-15 16:23:06 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -23,7 +23,7 @@ async def on_member_join(member):
             description=f'Hey {member.mention}, welcome to the ring! Get ready to fight!',
             color=discord.Color.red()
         )
-        embed.set_image(url="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjExNDMwJnc9YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7TKMGpxxHOGTdzJC/giphy.gif")
+        embed.set_image(url="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjExNDMwJnc9YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/l41lTjJp90Yy7W1Hq/giphy.gif")
         await channel.send(content=f'New challenger: {member.mention}', embed=embed)
 
 # Goodbye message
@@ -36,7 +36,7 @@ async def on_member_remove(member):
             description=f'{member.name} has left the ring. See you next time!',
             color=discord.Color.dark_grey()
         )
-        embed.set_image(url="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjExNDMwJnc9YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o7TKMGpxxHOGTdzJC/giphy.gif")
+        embed.set_image(url="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjExNDMwJnc9YyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/26AHtn0v1Qo78o1qE/giphy.gif")
         await channel.send(embed=embed)
 
 # Token dalla variabile di ambiente
